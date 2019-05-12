4a0c4b5ae7c746a4cf181024ca140a846721bd0c
Anthony Nock <nock.anthony@yahoo.com>
2019-05-11 22:47:37 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,16 @@
+var $= function (id) {
+    return document.getElementById(id);
+
+}
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var guess = Math.floor(Math.random()*4)
+var answer = letters[guess]
+var guessCount = 9;
+var userLetters = "";
+
+</script>
+
+var setup = function(id){
+
+    for (var i=0; i < answer.length; i++)
+    }
